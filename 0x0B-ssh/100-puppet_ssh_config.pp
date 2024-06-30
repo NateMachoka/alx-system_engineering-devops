f8e97776fcea5dcfd6aee2439b543ef028e61f01
Nathan Machoka <natemachoka@gmail.com>
2024-06-29 20:13:39 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,41 +1,13 @@
-file { '/home/ubuntu/.ssh/config':
+# Puppet script that ensures ssh configurations are okay
+
+file_line { 'Turn off passwd auth':
   ensure => 'present',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0600',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => '  IdentityFile ~/.ssh/school',
-  match => '^\s*IdentityFile\s+',
   ensure => 'present',
-}
-
-file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => '  PasswordAuthentication no',
-  match => '^\s*PasswordAuthentication\s+',
-  ensure => 'present',
-}
-
-file_line { 'Start Host block':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'Host myserver',
-  match => '^Host\s+myserver',
-  ensure => 'present',
-}
-
-file_line { 'Declare HostName':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => '  HostName 18.209.178.235',
-  match => '^\s*HostName\s+',
-  ensure => 'present',
-}
-
-file_line { 'Declare User':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => '  User ubuntu',
-  match => '^\s*User\s+',
-  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
