337f2a871d02ca3916bfb803141cf4b96df52f3d
Nathan Machoka <natemachoka@gmail.com>
2024-08-13 08:11:27 -0700

    fixed pycode 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,7 +20,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         'User-Agent': ('python:subreddit.subscriber.count:v1.0 '
-                   '(by /u/yourusername)')
+                       '(by /u/yourusername)')
         }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
